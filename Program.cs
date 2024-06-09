4b7191e73299e78bbbfcf84029cade2644020c8f
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2024-06-09 11:32:53 +0200

    Getting black and white array

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using ZXing;
+using ZXing.QrCode;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -8,8 +11,21 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.MapGet("/qrcode", (string text) => {
+    var writer = new QRCodeWriter();
+    var matrix = writer.encode(text, BarcodeFormat.QR_CODE, 500,
+             500, null);
+    List<List<bool>> table = new List<List<bool>>();
+    for (int y = 0; y < matrix.Height; y += 1)
+    {
+        var row = new List<bool>();
+        for (int x = 0; x < matrix.Width; x += 1)
+        {
+            row.Add(matrix[x, y]);
+        }
+        table.Add(row);
+    }
     return new {
-        text
+        table
     };
 });
 

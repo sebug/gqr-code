387c43b856482347c66776f58f5f6403c5393271
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2024-06-09 11:40:45 +0200

    Scannable QR code

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,9 +1,26 @@
 const transformTextBox = document.querySelector('#totransform');
+const cvs = document.querySelector('canvas');
+
+console.log(cvs);
+
+const paintQrCodeTable = (table) => {
+    let ctx = cvs.getContext('2d');
+    ctx.clearRect(0, 0, 500, 500);
+    for (let y = 0; y < table.length; y += 1) {
+        let row = table[y];
+        for (let x = 0; x < row.length; x += 1) {
+            if (row[x]) {
+                ctx.fillStyle = 'black';
+                ctx.fillRect(x, y, 1, 1);
+            }
+        }
+    }
+};
 
 const getQrCode = async (url) => {
     const responseText = await fetch('/qrcode?text=' + encodeURIComponent(url));
     const responseObject = await responseText.json();
-    console.log(responseObject);
+    paintQrCodeTable(responseObject.table);
 };
 
 transformTextBox.addEventListener('change', ev => {
